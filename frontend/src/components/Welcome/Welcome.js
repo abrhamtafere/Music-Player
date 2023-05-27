94d2bd31d1d2b376770ec31259acd0355b754b1e
abrhamtafere <tafereab@gmail.com>
2023-05-27 07:28:56 +0300

    updated

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -26,7 +26,9 @@ const Welcome = () => {
             <WelcomeText>
               Turn up the volume, hear the beat, Our website's got what you
               need, Find your rhythm, dance and sway, Let our music light up
-              your day, Join our community, let's play! Overall, we believe that
+              your day, Join our community, let's play! 
+              <br/><br/>
+              Overall, we believe that
               our music playing website provides the ultimate listening
               experience and is the best choice for anyone who loves music.
             </WelcomeText>
